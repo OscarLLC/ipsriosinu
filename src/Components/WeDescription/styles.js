26b74a1e337b63731accb3d9fa246ff366a697b4
oscar lopez <oscarlopez88991@gmail.com>
2021-03-19 11:39:52 -0500

    settings wedescription

--- src/Components/WeDescription/styles.js
+++ src/Components/WeDescription/styles.js
@@ -3,9 +3,16 @@ import styled from 'styled-components'
 
 export const ContentWD = styled.div`
     margin: 45px 90px;
+    @media(max-width: 680px){
+        margin: 45px 35px;
+    }
 
     .text-main-description{
         margin: 35px 0px;
+    }
+
+    h2{
+        margin: 15px 0px; 
     }
 
     ul{
