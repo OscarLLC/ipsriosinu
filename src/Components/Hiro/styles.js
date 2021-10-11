d3bfffb3ddcf60558e274b93bb0482871979a4de
oscar lopez <oscarlopez88991@gmail.com>
2021-10-10 21:55:13 -0500

    code load

--- src/Components/Hiro/styles.js
+++ src/Components/Hiro/styles.js
@@ -16,7 +16,7 @@ export const ContentHiro = styled.section`
         align-items: center;
         justify-content: center;
         /* background-color: #26b4c0; */
-        /**esto lo ahgo para dejar subir a lxxxxccclllddlkeroku ok mas  **/
+        /**esto lo ahgo para dejar subir a lxxxxcddddcclllddlkeroku ok mas  **/
         background-color: #ff0080;
         text-align: center;
         cursor: pointer;
