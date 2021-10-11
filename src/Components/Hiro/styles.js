7d418a5513a0a799782bfced457415722e0f7bc5
oscar lopez <oscarlopez88991@gmail.com>
2021-10-10 19:50:19 -0500

    load

--- src/Components/Hiro/styles.js
+++ src/Components/Hiro/styles.js
@@ -16,6 +16,7 @@ export const ContentHiro = styled.section`
         align-items: center;
         justify-content: center;
         /* background-color: #26b4c0; */
+        /**esto lo ahgo para dejar subir a keroku **/
         background-color: #ff0080;
         text-align: center;
         cursor: pointer;
